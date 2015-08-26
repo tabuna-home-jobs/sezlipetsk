4935afc0448f2a861cbe1eb10fb20fef9a0e5c02
kudaeva <angelforevor@mail.ru>
2015-08-26 17:02:56 +0300

    изменения на странице управляющая компания

--- about/upravlyayushchaya-kompaniya/index.php
+++ about/upravlyayushchaya-kompaniya/index.php
@@ -35,7 +35,7 @@ $APPLICATION->SetTitle("Управляющая компания");
 						</div>
 					</div>
 					<div class="hidden-xs hidden-sm right_part col-xs-12 col-sm-12 col-md-5">
-						<img src="img/building.jpg">
+						<img src="<?=SITE_TEMPLATE_PATH?>/img/building.jpg">
 					</div>
 				</div>
 			</div>
@@ -45,26 +45,26 @@ $APPLICATION->SetTitle("Управляющая компания");
 		<section class="what_we_are row">
 			<div class="row">
 				<div class="col-md-3 item_company_action row ">
-					<div class="compan_action_img col-md-12 col-xs-3"><img src="img/marker.png"></div>
+					<div class="compan_action_img col-md-12 col-xs-3"><img src="<?=SITE_TEMPLATE_PATH?>/img/marker.png"></div>
 					<div class="col-md-12 col-xs-9">Мы формируем в особых экономических зонах новые территории
 					                                экономического развития и социального роста для последующего масштабирования
 					                                в рамках всей страны.
 					</div>
 				</div>
 				<div class="col-md-3 item_company_action row ">
-					<div class="compan_action_img col-md-12 col-xs-3"><img src="img/two.png"></div>
+					<div class="compan_action_img col-md-12 col-xs-3"><img src="<?=SITE_TEMPLATE_PATH?>/img/two.png"></div>
 					<div class="col-md-12 col-xs-9">Мы привлекаем инвесторов с перспективными технологиями производства и
 					                                управления, создавая рабочие места в реальном секторе экономике.
 					</div>
 				</div>
 				<div class="col-md-3 item_company_action row ">
-					<div class="compan_action_img col-md-12 col-xs-3"><img src="img/cup.png"></div>
+					<div class="compan_action_img col-md-12 col-xs-3"><img src="<?=SITE_TEMPLATE_PATH?>/img/cup.png"></div>
 					<div class="col-md-12 col-xs-9">Мы ориентируемся на передовые мировые стандарты, стремясь стать одной из
 					                                лучших в мире компаний в области управления особыми экономическими зонами.
 					</div>
 				</div>
 				<div class="col-md-3 item_company_action row ">
-					<div class="compan_action_img col-md-12 col-xs-3"><img src="img/mebel.png"></div>
+					<div class="compan_action_img col-md-12 col-xs-3"><img src="<?=SITE_TEMPLATE_PATH?>/img/mebel.png"></div>
 					<div class="col-md-12 col-xs-9">Мы создаем комфортные условия для ведения бизнеса.</div>
 				</div>
 			</div>
@@ -75,38 +75,38 @@ $APPLICATION->SetTitle("Управляющая компания");
 
 			<div class="row cennost_items">
 				<div class="col-md-4">
-					<div class="cennost_name"><img src="img/ok.png">Ответственность</div>
+					<div class="cennost_name"><img src="<?=SITE_TEMPLATE_PATH?>/img/ok.png">Ответственность</div>
 					<div class="cennost_body">Мы развиваем особые экономические зоны с полным осознанием своей
 					                          ответственности перед инвесторами, гражданами и налогоплательщиками.
 					</div>
 				</div>
 				<div class="col-md-4">
-					<div class="cennost_name"><img src="img/ok.png">Патриотизм</div>
+					<div class="cennost_name"><img src="<?=SITE_TEMPLATE_PATH?>/img/ok.png">Патриотизм</div>
 					<div class="cennost_body">Мы любим Россию. Мы понимаем, что экономическая мощь и безопасность России,
 					                          благосостояние общества и уверенность в завтрашнем дне во многом зависит и от нас.
 					</div>
 				</div>
 				<div class="col-md-4">
-					<div class="cennost_name"><img src="img/ok.png">Лидерство</div>
+					<div class="cennost_name"><img src="<?=SITE_TEMPLATE_PATH?>/img/ok.png">Лидерство</div>
 					<div class="cennost_body">Мы стремимся быть лучшими в том, что делаем. Высшая форма лидерства – это
 					                          успех. Добиваясь лидерства, мы делимся успехом с нашими партне-
 					                          рами, с теми, кто доверяет нам и кому доверяем мы.
 					</div>
 				</div>
 				<div class="col-md-4">
-					<div class="cennost_name"><img src="img/ok.png">Единство</div>
+					<div class="cennost_name"><img src="<?=SITE_TEMPLATE_PATH?>/img/ok.png">Единство</div>
 					<div class="cennost_body">Мы - команда, сила которой во взаимной поддержке и уважении мнения каждого.
 					</div>
 				</div>
 				<div class="col-md-4">
-					<div class="cennost_name"><img src="img/ok.png">Открытость</div>
+					<div class="cennost_name"><img src="<?=SITE_TEMPLATE_PATH?>/img/ok.png">Открытость</div>
 					<div class="cennost_body">Мы активно участвуем в процессе межкультурного взаимодействия. Привлекая к
 					                          партнерству иностранные компании, мы перенимаем лучший мировой опыт по организации и управлению
 					                          инновационной экономикой..
 					</div>
 				</div>
 				<div class="col-md-4">
-					<div class="cennost_name"><img src="img/ok.png">Развитие</div>
+					<div class="cennost_name"><img src="<?=SITE_TEMPLATE_PATH?>/img/ok.png">Развитие</div>
 					<div class="cennost_body">Развитие – это совершенствование через последовательное решение задач и
 					                          достижение поставленных целей. Развиваясь, мы изменяемся. Изменяя настоящее, мы создаем будущее.
 					</div>
@@ -133,7 +133,7 @@ $APPLICATION->SetTitle("Управляющая компания");
 								<div class="block_icon_arrow">
 									<span class="glyphicon glyphicon-menu-right"></span>
 								</div>
-								<img src="img/inform.jpg">
+								<img src="<?=SITE_TEMPLATE_PATH?>/img/inform.jpg">
 							</div>
 						</div>
 					</a>
@@ -149,7 +149,7 @@ $APPLICATION->SetTitle("Управляющая компания");
 								<div class="block_icon_arrow">
 									<span class="glyphicon glyphicon-menu-right"></span>
 								</div>
-								<img src="img/inform1.jpg">
+								<img src="<?=SITE_TEMPLATE_PATH?>/img/inform1.jpg">
 							</div>
 						</div>
 					</a>
