36e8f8f581ad9006c8235274ddb0834e3a61c3f9
kudaeva <angelforevor@mail.ru>
2015-08-26 14:27:20 +0300

    Вывела новости на страницу о проэкте

--- about/index.php
+++ about/index.php
@@ -110,18 +110,69 @@ $APPLICATION->SetTitle("О проекте");
 			</div>
 			<!--Последняя новость на главной-->
 			<?$APPLICATION->IncludeComponent(
-				"custom:news.list",
-				"about_news",
-				Array(
-					"DISPLAY_DATE" => "Y",
-					"DISPLAY_PICTURE" => "Y",
-					"DISPLAY_PREVIEW_TEXT" => "Y",
-					"IBLOCK_TYPE" => "news",
-					"IBLOCK_ID" => "infoportal_news_s1",
-					"NEWS_COUNT" => "2",
-					"PREVIEW_TRUNCATE_LEN" => "100",
-				)
-			);?>
+	"custom:news.list", 
+	"about_news", 
+	array(
+		"DISPLAY_DATE" => "Y",
+		"DISPLAY_PICTURE" => "Y",
+		"DISPLAY_PREVIEW_TEXT" => "Y",
+		"IBLOCK_TYPE" => "news",
+		"IBLOCK_ID" => "infoportal_news_s1",
+		"NEWS_COUNT" => "2",
+		"PREVIEW_TRUNCATE_LEN" => "100",
+		"COMPONENT_TEMPLATE" => "about_news",
+		"SORT_BY1" => "ACTIVE_FROM",
+		"SORT_ORDER1" => "DESC",
+		"SORT_BY2" => "SORT",
+		"SORT_ORDER2" => "ASC",
+		"FILTER_NAME" => "",
+		"FIELD_CODE" => array(
+			0 => "",
+			1 => "",
+		),
+		"PROPERTY_CODE" => array(
+			0 => "",
+			1 => "",
+		),
+		"CHECK_DATES" => "Y",
+		"DETAIL_URL" => "",
+		"AJAX_MODE" => "N",
+		"AJAX_OPTION_JUMP" => "N",
+		"AJAX_OPTION_STYLE" => "Y",
+		"AJAX_OPTION_HISTORY" => "N",
+		"AJAX_OPTION_ADDITIONAL" => "undefined",
+		"CACHE_TYPE" => "A",
+		"CACHE_TIME" => "36000000",
+		"CACHE_FILTER" => "N",
+		"CACHE_GROUPS" => "Y",
+		"ACTIVE_DATE_FORMAT" => "d.m.Y",
+		"SET_TITLE" => "N",
+		"SET_BROWSER_TITLE" => "N",
+		"SET_META_KEYWORDS" => "N",
+		"SET_META_DESCRIPTION" => "N",
+		"SET_LAST_MODIFIED" => "N",
+		"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+		"ADD_SECTIONS_CHAIN" => "N",
+		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+		"PARENT_SECTION" => "",
+		"PARENT_SECTION_CODE" => "",
+		"INCLUDE_SUBSECTIONS" => "Y",
+		"DISPLAY_NAME" => "Y",
+		"PAGER_TEMPLATE" => ".default",
+		"DISPLAY_TOP_PAGER" => "N",
+		"DISPLAY_BOTTOM_PAGER" => "Y",
+		"PAGER_TITLE" => "Новости",
+		"PAGER_SHOW_ALWAYS" => "N",
+		"PAGER_DESC_NUMBERING" => "N",
+		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+		"PAGER_SHOW_ALL" => "N",
+		"PAGER_BASE_LINK_ENABLE" => "N",
+		"SET_STATUS_404" => "N",
+		"SHOW_404" => "N",
+		"MESSAGE_404" => ""
+	),
+	false
+);?>
 			<!--Последняя новость на главной-->
 
 
