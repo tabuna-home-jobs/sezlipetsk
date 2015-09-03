7a1513169ae6a2b6021d11e06d519f29a571d85b
kudaeva <angelforevor@mail.ru>
2015-09-03 13:01:40 +0300

    инфоблоки

--- bitrix/components/custom/news.list_connect/templates/.default/template.php
+++ bitrix/components/custom/news.list_connect/templates/.default/template.php
@@ -10,6 +10,13 @@
 /** @var string $templateFolder */
 /** @var string $componentPath */
 /** @var CBitrixComponent $component */
+	function extractFileName($filename) {
+		$p = strrpos($filename, '.');
+		if ($p > 0) return substr($filename, 0, $p);
+		else return $filename;
+	}
+
+
 $this->setFrameMode(true);
 	//var_dump($arResult);
 	//die('ghjkl;');
@@ -91,11 +98,11 @@ $this->setFrameMode(true);
 
 
 <?
-$i = 0;
+	$i = 0;
 	$big_panel = "";//переменнаяя для хранения разделов из главной  панели
 for($i = 0; $i<count($arResult["razdel"]); $i++){
-	if($i == 0){$flag = 'true';}else {$flag = 'false';}
-	$razdel_tmp = '<li role="presentation" class="">
+	if($i == 0){$flag = 'true'; $class="active"; $fade='active in'; }else {$flag = 'false'; $class=""; $fade = ''; }
+	$razdel_tmp = '<li role="presentation" class="'.$class.'">
                 <a href="#r'.$arResult["razdel"][$i]['ID'].'" role="tab" id="'.$arResult["razdel"][$i]['ID'].'-tab" data-toggle="tab" aria-controls="r'.$arResult["razdel"][$i]['ID'].'"
                    aria-expanded="'.$flag.'">
                 <span>
@@ -107,10 +114,61 @@ for($i = 0; $i<count($arResult["razdel"]); $i++){
                 </a>
             </li>';
 	$big_panel.=$razdel_tmp;
+
+	$tmp_start_panel = '<div role="tabpanel" class="tab-pane fade '.$fade.'" id="r'.$arResult["razdel"][$i]['ID'].'" aria-labelledby="r'.$arResult["razdel"][$i]['ID'].'-tab"> <div class="row">';
+	$tmp_stop_panel = '</div></div>';
+
+	$tmp_start_panel_vn ='<div class="col-xs-6 left-side-4-trigger">
+                        <!--Внутренняя навигация -->
+                        <ul id="r'.$arResult["razdel"][$i]['ID'].'-tabs" class="nav nav-tabs navigacia" role="tablist">';
+	$tmp_stop_panel_vn = '</ul></div>';
+
+	$tmp_elements_start ='<div class="col-xs-6 innerpanel right-side-trigger"><div id="electro_panel" class="tab-content">';
+	$tmp_elements_stop = '</div></div>';
+	$tmp_panel_cild = '';
+	for($j = 0; $j < count($arResult["razdel"][$i]['child']); $j++ ){
+		if($i == 0){$flag2 = 'true'; $class2="active"; }else {$flag2 = 'false'; $class2="";}
+		$tmp_panel_cild = '<li role="presentation" class="'.$class.'"><a href="#r'.$arResult["razdel"][$i]['child'][$j]['ID'].'" id="electro1-tab" role="tab"
+                                                                      data-toggle="tab"
+                                                                      aria-controls="'.$arResult["razdel"][$i]['child']['ID'].'" aria-expanded="'.$flag2.'">
+                                <span>
+                                    <img  src="'.$arResult["razdel"][$i]['child'][$j]['UF_SECOND_IMG']['SRC'].'">
+                                    <img src="'.$arResult["razdel"][$i]['child'][$j]['UF_FIRST_IMG']['SRC'].'">
+                                </span>
+                                '.$arResult["razdel"][$i]['child'][$j]['NAME'].'
+                            </li>';
+		$vn_tmp .= $tmp_panel_cild;
+		$tmp_element = "";
+		for($k = 0; $k<count($arResult["razdel"][$i]['child'][$j]['element']); $k++ ){
+			$files_str = "";
+			for($m = 0; $m<count($arResult["razdel"][$i]["child"][$j]['element'][$k]['files']); $m++){
+				$files_str.='<div class="item_download">
+                                        <div class="item_download_type"><img src="'.SITE_TEMPLATE_PATH.'/img/pdf.png">DOC,'.$arResult["razdel"][$i]["child"][$j]['element'][$k]['files'][$m]["FILE_SIZE"].' КБ</div>
+                                        <div class="item_download_date">'.$arResult["razdel"][$i]["child"][$j]["element"][$k]['DATE'].'</div>
+                                        <div class="item_download_name">'.extractFileName($arResult["razdel"][$i]["child"][$j]['element'][$k]['files'][$m]["ORIGINAL_NAME"]).'</div>
+                                    </div>';
+			}
+			$tmp_element = '    <div role="tabpanel" class="tab-pane fade active in " id="r'.$arResult["razdel"][$i]['child'][$j]['ID'].'"
+                                 aria-labelledby="electro1-tab">
+                                <div class=""><h3>'.$arResult["razdel"][$i]["child"][$j]["element"][$k] ["row"]['NAME'].'</h3>
+									'.$files_str.'
+                                </div>
+                            </div>';
+			//var_dump($arResult["razdel"][$i]["child"][$j]["element"]);
+			//die('fghj');
+			$tmp_elements.=$tmp_element;
+		}
+	}
+	$vn = $tmp_start_panel_vn.$vn_tmp.$tmp_stop_panel_vn;//Собираем внутреннюю навигацию
+	$elements = $tmp_elements_start.$tmp_elements.$tmp_elements_stop;//Собираем внутреннюю панель навигании
+	$pan.= $tmp_start_panel.$vn.$elements.$tmp_stop_panel;
 	//var_dump($razdel_tmp);
 
 
 }
+
+
+	//var_dump($pan);
 	//die('fghjk');
 
 
@@ -120,257 +178,13 @@ for($i = 0; $i<count($arResult["razdel"]); $i++){
 	<!-- #Главная навигация# -->
 	<ul id="myTabs" class="nav nav-tabs nav_galery nav-technology" role="tablist">
 		<?=$big_panel?>
-		<!--<li role="presentation" class="active"><a href="#electro" id="electro-tab" role="tab" data-toggle="tab"
-		                                          aria-controls="electro" aria-expanded="true">
-                <span>
-                        <img src="<?=SITE_TEMPLATE_PATH?>/img/icons/T_14(1)white.png">
-                        <img class="active_img" src="<?=SITE_TEMPLATE_PATH?>/img/icons/T_14(1).png">
-                    </span>
-				Электроснабжение</a>
-
-
-
-
-		</li>
-		<li role="presentation" class="">
-			<a href="#teplo" role="tab" id="teplo-tab" data-toggle="tab" aria-controls="teplo"
-			   aria-expanded="false">
-                <span>
-                        <img src="<?=SITE_TEMPLATE_PATH?>/img/icons/T_14(4)white.png">
-                        <img class="active_img" src="<?=SITE_TEMPLATE_PATH?>/img/icons/T_14(4).png">
-                    </span>
-				Теплоснабжение
-			</a>
-		</li>
-		<li role="presentation" class="">
-			<a href="#teplo" role="tab" id="teplo-tab" data-toggle="tab" aria-controls="teplo"
-			   aria-expanded="false">
-                <span>
-                        <img src="<?=SITE_TEMPLATE_PATH?>/img/icons/T_17(1)white.png">
-                        <img class="active_img" src="<?=SITE_TEMPLATE_PATH?>/img/icons/T_17(1).png">
-                    </span>
-				Газоснабжение
-			</a>
-		</li>
-		<li role="presentation" class="">
-			<a href="#teplo" role="tab" id="teplo-tab" data-toggle="tab" aria-controls="teplo"
-			   aria-expanded="false">
-                <span>
-                        <img src="<?=SITE_TEMPLATE_PATH?>/img/icons/T_17(2)white.png">
-                        <img class="active_img" src="<?=SITE_TEMPLATE_PATH?>/img/icons/T_17(2).png">
-                    </span>
-				Водоснабжение
-			</a>
-		</li>
-		<li role="presentation" class="">
-			<a href="#teplo" role="tab" id="teplo-tab" data-toggle="tab" aria-controls="teplo"
-			   aria-expanded="false">
-                <span>
-                        <img src="<?=SITE_TEMPLATE_PATH?>/img/icons/T_17(3)white.png">
-                        <img class="active_img" src="<?=SITE_TEMPLATE_PATH?>/img/icons/T_17(3).png">
-                    </span>
-				Водоотведение
-			</a>
-		</li>
-		<li role="presentation" class="">
-			<a href="#teplo" role="tab" id="teplo-tab" data-toggle="tab" aria-controls="teplo"
-			   aria-expanded="false">
-                <span>
-                        <img src="<?=SITE_TEMPLATE_PATH?>/img/icons/vyshka.png">
-                        <img class="active_img" src="<?=SITE_TEMPLATE_PATH?>/img/icons/vyshka_2.png">
-                    </span>
-				Телекоммуникации
-			</a>
-		</li>-->
 	</ul>
 	<!-- #Главная навигация# -->
 
 	<!-- Панели навигации -->
 	<div id="main_content_tabs" class="tab-content">
-		<!--панель1 -->
-		<div role="tabpanel" class="tab-pane fade active in" id="electro" aria-labelledby="electro-tab">
-			<div class="row">
-				<div class="col-xs-6 left-side-4-trigger">
-					<!--Внутренняя навигация -->
-					<ul id="electro_tabs" class="nav nav-tabs navigacia" role="tablist">
-						<li role="presentation" class="active"><a href="#electro1" id="electro1-tab" role="tab"
-						                                          data-toggle="tab"
-						                                          aria-controls="electro1" aria-expanded="true">
-                                <span>
-                                    <img  src="<?=SITE_TEMPLATE_PATH?>/img/icons/T_04(1)white.png">
-                                    <img src="<?=SITE_TEMPLATE_PATH?>/img/icons/T_04(1).png">
-                                </span>
-								Общая информация по технологическому
-								присоединению/подключению</a>
-						</li>
-						<li role="presentation" class="">
-							<a href="#electro2" role="tab" id="electro2-tab"    data-toggle="tab" aria-controls="electro2" aria-expanded="false">
-                                <span>
-                                    <img  src="<?=SITE_TEMPLATE_PATH?>/img/icons/123_2.png">
-                                    <img src="<?=SITE_TEMPLATE_PATH?>/img/icons/123.png">
 
-                                </span>
-								Порядок по технологическому присоединению/подключению</a></li>
-						<li role="presentation" class=""><a href="#electro2" role="tab" id="electro2-tab"
-						                                    data-toggle="tab"
-						                                    aria-controls="electro2" aria-expanded="false">
-                                <span>
-                                    <img src="<?=SITE_TEMPLATE_PATH?>/img/icons/T_04(3)white.png">
-                                    <img  src="<?=SITE_TEMPLATE_PATH?>/img/icons/T_04(3).png">
-                                </span>
-								Типовые формы документов</a></li>
-						<li role="presentation" class=""><a href="#electro2" role="tab" id="electro2-tab"
-						                                    data-toggle="tab"
-						                                    aria-controls="electro2" aria-expanded="false">
-                                <span>
-                                    <img src="<?=SITE_TEMPLATE_PATH?>/img/icons/ruble.png">
-                                    <img  src="<?=SITE_TEMPLATE_PATH?>/img/icons/ruble_2.png">
-                                </span>
-								Тарифы на технологическое присоединение/подключение</a></li>
-						<li role="presentation" class=""><a href="#electro2" role="tab" id="electro2-tab"
-						                                    data-toggle="tab"
-						                                    aria-controls="electro2" aria-expanded="false">
-                                <span>
-                                     <img  src="<?=SITE_TEMPLATE_PATH?>/img/icons/mc2.png">
-                                    <img src="<?=SITE_TEMPLATE_PATH?>/img/icons/mc2_2.png">
-
-                                </span>
-								Сведения о наличии мощности, свободной для технологического присоединения/ подключения,
-								а так же о заявках и договорах</a></li>
-						<li role="presentation" class=""><a href="#electro2" role="tab" id="electro2-tab"
-						                                    data-toggle="tab"
-						                                    aria-controls="electro2" aria-expanded="false">
-                                <span>
-                                    <img src="<?=SITE_TEMPLATE_PATH?>/img/icons/T_04(5)white.png">
-                                    <img  src="<?=SITE_TEMPLATE_PATH?>/img/icons/T_04(5).png">
-                                </span>
-								Нормативные документы</a></li>
-					</ul>
-					<!--Внутренняя навигация -->
-
-				</div>
-
-				<div class="col-xs-6 innerpanel right-side-trigger">
-
-					<!--Внутренние панели навигации-->
-					<div id="electro_panel" class="tab-content">
-						<div role="tabpanel" class="tab-pane fade active in " id="electro1"
-						     aria-labelledby="electro1-tab">
-							<div class=""><h3>Общая информация по технологическому присоединению/
-							                  подключению</h3>
-
-								<div class="item_download">
-									<div class="item_download_type"><img src="<?=SITE_TEMPLATE_PATH?>/img/pdf.png">DOC, 105 КБ</div>
-									<div class="item_download_date">30.09.2013</div>
-									<div class="item_download_name">Положение о закупках ОАО «ОЭЗ ППТ «Липецк»</div>
-								</div>
-								<div class="item_download">
-									<div class="item_download_type"><img src="<?=SITE_TEMPLATE_PATH?>/img/pdf.png">DOC, 105 КБ</div>
-									<div class="item_download_date">30.09.2013</div>
-									<div class="item_download_name">Положение о закупках ОАО «ОЭЗ ППТ «Липецк»</div>
-								</div>
-								<div class="item_download">
-									<div class="item_download_type"><img src="<?=SITE_TEMPLATE_PATH?>/img/pdf.png">DOC, 105 КБ</div>
-									<div class="item_download_date">30.09.2013</div>
-									<div class="item_download_name">Положение о закупках ОАО «ОЭЗ ППТ «Липецк»</div>
-								</div>
-							</div>
-						</div>
-						<div role="tabpanel" class="tab-pane fade" id="electro2" aria-labelledby="electro2-tab">
-							<h3>Порядок по технологическому присоединению/подключению</h3>
-
-							<div class="item_download">
-								<div class="item_download_type"><img src="<?=SITE_TEMPLATE_PATH?>/img/pdf.png">DOC, 105 КБ</div>
-								<div class="item_download_date">30.09.2013</div>
-								<div class="item_download_name">Положение о закупках ОАО «ОЭЗ ППТ «Липецк»</div>
-							</div>
-							<div class="item_download">
-								<div class="item_download_type"><img src="<?=SITE_TEMPLATE_PATH?>/img/pdf.png">DOC, 105 КБ</div>
-								<div class="item_download_date">30.09.2013</div>
-								<div class="item_download_name">Положение о закупках ОАО «ОЭЗ ППТ «Липецк»</div>
-							</div>
-							<div class="item_download">
-								<div class="item_download_type"><img src="<?=SITE_TEMPLATE_PATH?>/img/pdf.png">DOC, 105 КБ</div>
-								<div class="item_download_date">30.09.2013</div>
-								<div class="item_download_name">Положение о закупках ОАО «ОЭЗ ППТ «Липецк»</div>
-							</div>
-						</div>
-					</div>
-					<!--Внутренние панели навигации-->
-
-				</div>
-			</div>
-		</div>
-		<!--панель1 -->
-
-		<!--панель2 -->
-		<div role="tabpanel" class="tab-pane fade" id="teplo" aria-labelledby="teplo-tab">
-			<div class="row">
-				<div class="col-xs-6 left-side-4-trigger">
-					<ul id="teplo_tabs" class="nav nav-tabs navigacia" role="tablist">
-						<li role="presentation" class="active">
-							<a href="#teplo1" id="teplo1-tab" role="tab" data-toggle="tab" aria-controls="teplo1" aria-expanded="true">
-								<img src="<?=SITE_TEMPLATE_PATH?>/img/pen_invert.png" class="hidden-xs">
-								Типовые формы документов
-							</a>
-						</li>
-						<li role="presentation" class=""><a href="#teplo2" role="tab" id="teplo2-tab"
-						                                    data-toggle="tab"
-						                                    aria-controls="teplo2" aria-expanded="false"><img
-									src="<?=SITE_TEMPLATE_PATH?>/img/ruble_invert.png" class="hidden-xs">Тарифы на технологическое
-						                                                         присоединение/подключение</a></li>
-					</ul>
-				</div>
-				<div class="col-xs-6 innerpanel right-side-trigger">
-					<div id="teplo_right" class="tab-content">
-						<div role="tabpanel" class="tab-pane fade active in" id="teplo1"
-						     aria-labelledby="teplo1-tab">
-							<h3>Общая информация по технологическому присоединению/
-							    подключению</h3>
-
-							<div class="item_download">
-								<div class="item_download_type"><img src="<?=SITE_TEMPLATE_PATH?>/img/pdf.png">DOC, 105 КБ</div>
-								<div class="item_download_date">30.09.2013</div>
-								<div class="item_download_name">Положение о закупках ОАО «ОЭЗ ППТ «Липецк»</div>
-							</div>
-							<div class="item_download">
-								<div class="item_download_type"><img src="<?=SITE_TEMPLATE_PATH?>/img/pdf.png">DOC, 105 КБ</div>
-								<div class="item_download_date">30.09.2013</div>
-								<div class="item_download_name">Положение о закупках ОАО «ОЭЗ ППТ «Липецк»</div>
-							</div>
-							<div class="item_download">
-								<div class="item_download_type"><img src="<?=SITE_TEMPLATE_PATH?>/img/pdf.png">DOC, 105 КБ</div>
-								<div class="item_download_date">30.09.2013</div>
-								<div class="item_download_name">Положение о закупках ОАО «ОЭЗ ППТ «Липецк»</div>
-							</div>
-						</div>
-
-						<div role="tabpanel" class="tab-pane fade" id="teplo2" aria-labelledby="teplo2-tab">
-							<h3>Общая информация по технологическому присоединению/
-							    подключению</h3>
-
-							<div class="item_download">
-								<div class="item_download_type"><img src="<?=SITE_TEMPLATE_PATH?>/img/pdf.png">DOC, 105 КБ</div>
-								<div class="item_download_date">30.09.2013</div>
-								<div class="item_download_name">Положение о закупках ОАО «ОЭЗ ППТ «Липецк»</div>
-							</div>
-							<div class="item_download">
-								<div class="item_download_type"><img src="<?=SITE_TEMPLATE_PATH?>/img/pdf.png">DOC, 105 КБ</div>
-								<div class="item_download_date">30.09.2013</div>
-								<div class="item_download_name">Положение о закупках ОАО «ОЭЗ ППТ «Липецк»</div>
-							</div>
-							<div class="item_download">
-								<div class="item_download_type"><img src="<?=SITE_TEMPLATE_PATH?>/img/pdf.png">DOC, 105 КБ</div>
-								<div class="item_download_date">30.09.2013</div>
-								<div class="item_download_name">Положение о закупках ОАО «ОЭЗ ППТ «Липецк»</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<!--панель2 -->
-
+		<?=$pan?>
 	</div>
 	<!--Панели навигации -->
 </section>
