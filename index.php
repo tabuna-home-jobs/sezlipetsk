b1ab67ba1de216802943d5860946e06e822b4ea1
kudaeva <angelforevor@mail.ru>
2015-09-08 17:28:56 +0300

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	upload/resize_cache/iblock/9f2/298_221_1/9f29473a31265f30942504d9b3f96cad.jpg

--- index.php
+++ index.php
@@ -227,13 +227,82 @@ $GLOBALS["arrFilterMain"] = array("PROPERTY_MAIN_VALUE" => 1);
   <!--Последняя новость на главной-->
   <div class="col-lg-3 col-md-12 col-sm-12 col-xs-12 row block_stats">
    <div class="col-xs-12 news_name">Целевые показатели роста</div>
-   <div class="col-xs-12 pokazateli"><div class="col-sm-6 col-xs-12 col-md-12 stat_img"><img src="<?=SITE_TEMPLATE_PATH?>/img/stat.jpg"></div>
-    <div class="col-sm-6 col-xs-12 col-md-12 stat_descript">
+   <div class="col-xs-12 pokazateli">
+	   <div class="col-sm-6 col-xs-12 col-md-12 stat_img">
+		   <!--Нижняя карусел партнеров-->
+		   <?$APPLICATION->IncludeComponent(
+			   "custom:news.list",
+			   "pokazat_rosta",
+			   array(
+				   "IBLOCK_ID" => "BOTTOM_CARUSEL",
+				   "COMPONENT_TEMPLATE" => "pokazat_rosta",
+				   "IBLOCK_TYPE" => "news",
+				   "NEWS_COUNT" => "20",
+				   "SORT_BY1" => "ACTIVE_FROM",
+				   "SORT_ORDER1" => "DESC",
+				   "SORT_BY2" => "SORT",
+				   "SORT_ORDER2" => "ASC",
+				   "FILTER_NAME" => "",
+				   "FIELD_CODE" => array(
+					   0 => "",
+					   1 => "",
+				   ),
+				   "PROPERTY_CODE" => array(
+					   0 => "",
+					   1 => "",
+				   ),
+				   "CHECK_DATES" => "Y",
+				   "DETAIL_URL" => "",
+				   "AJAX_MODE" => "N",
+				   "AJAX_OPTION_JUMP" => "N",
+				   "AJAX_OPTION_STYLE" => "Y",
+				   "AJAX_OPTION_HISTORY" => "N",
+				   "AJAX_OPTION_ADDITIONAL" => "undefined",
+				   "CACHE_TYPE" => "A",
+				   "CACHE_TIME" => "36000000",
+				   "CACHE_FILTER" => "N",
+				   "CACHE_GROUPS" => "N",
+				   "PREVIEW_TRUNCATE_LEN" => "",
+				   "ACTIVE_DATE_FORMAT" => "d.m.Y",
+				   "SET_TITLE" => "N",
+				   "SET_BROWSER_TITLE" => "N",
+				   "SET_META_KEYWORDS" => "N",
+				   "SET_META_DESCRIPTION" => "N",
+				   "SET_LAST_MODIFIED" => "N",
+				   "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+				   "ADD_SECTIONS_CHAIN" => "N",
+				   "HIDE_LINK_WHEN_NO_DETAIL" => "N",
+				   "PARENT_SECTION" => "",
+				   "PARENT_SECTION_CODE" => "",
+				   "INCLUDE_SUBSECTIONS" => "Y",
+				   "DISPLAY_DATE" => "Y",
+				   "DISPLAY_NAME" => "Y",
+				   "DISPLAY_PICTURE" => "Y",
+				   "DISPLAY_PREVIEW_TEXT" => "Y",
+				   "PAGER_TEMPLATE" => ".default",
+				   "DISPLAY_TOP_PAGER" => "N",
+				   "DISPLAY_BOTTOM_PAGER" => "N",
+				   "PAGER_TITLE" => "Новости",
+				   "PAGER_SHOW_ALWAYS" => "N",
+				   "PAGER_DESC_NUMBERING" => "N",
+				   "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+				   "PAGER_SHOW_ALL" => "N",
+				   "PAGER_BASE_LINK_ENABLE" => "N",
+				   "SET_STATUS_404" => "N",
+				   "SHOW_404" => "N",
+				   "MESSAGE_404" => ""
+			   ),
+			   false
+		   );?>
+		   <!--<img src="<?=SITE_TEMPLATE_PATH?>/img/stat.jpg">-->
+	   </div>
+    <!--<div class="col-sm-6 col-xs-12 col-md-12 stat_descript">
      <ul>
       <li><img src="<?=SITE_TEMPLATE_PATH?>/img/black_dot.png">реализация 1-го этапа проекта в Грязинском районе</li>
       <li><img src="<?=SITE_TEMPLATE_PATH?>/img/red_dot.png">после реализации 2-го этапа в Елецком районе</li>
      </ul>
-    </div></div>
+    </div>-->
+   </div>
   </div>
  </section>
 
