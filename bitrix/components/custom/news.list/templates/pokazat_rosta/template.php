979eb76076e765116c25e09f0303248071eb8232
kudaeva <angelforevor@mail.ru>
2015-09-09 10:23:46 +0300

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	upload/resize_cache/iblock/9f2/298_221_1/9f29473a31265f30942504d9b3f96cad.jpg
    
    Сделала ещё один слайдер на главной

--- bitrix/components/custom/news.list/templates/pokazat_rosta/template.php
+++ bitrix/components/custom/news.list/templates/pokazat_rosta/template.php
@@ -15,13 +15,13 @@ $this->setFrameMode(true);
 	$n = 0;
 	$str = "";
 ?>
-<div class="col-md-6 citata">
-	<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+<? ?>	<div class="citata pokazat_rosta ">
+	<div id="carousel-example-generic2" class="carousel slide" data-ride="carousel">
 		<!-- Indicators -->
-		<ol class="carousel-indicators">
+		<!--<ol class="carousel-indicators">
 			<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
 			<li data-target="#carousel-example-generic" data-slide-to="1"></li>
-		</ol>
+		</ol>-->
 
 		<!-- Wrapper for slides -->
 		<div class="carousel-inner" role="listbox">
@@ -60,13 +60,15 @@ $this->setFrameMode(true);
 		</div>
 
 		<!-- Controls -->
-		<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+		<a class="left carousel-control" href="#carousel-example-generic2" role="button" data-slide="prev">
 			<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
 			<span class="sr-only">Previous</span>
 		</a>
-		<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+		<a class="right carousel-control" href="#carousel-example-generic2" role="button" data-slide="next">
 			<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
 			<span class="sr-only">Next</span>
 		</a>
 	</div>
 </div>
+
+<?  ?>
