ba5afc13fdd47ecfda809519cdee947754c01709
mamau <mopckou_pak48rus@mail.ru>
2015-09-11 15:51:40 +0300

    Социалочги

--- bitrix/templates/oez/include/socials.php
+++ bitrix/templates/oez/include/socials.php
@@ -1,8 +1,12 @@
 <div class="col-lg-5 col-md-12 col-sm-6 col-xs-12 foot_item">
 	<div class="foot_name">Соц сети</div>
 	<div class="social">
-		<a><img src="<?=SITE_TEMPLATE_PATH?>/img/face.png"></a>
-		<a><img src="<?=SITE_TEMPLATE_PATH?>/img/twitter.png"></a>
-		<a><img src="<?=SITE_TEMPLATE_PATH?>/img/youtube.png"></a>
+		<a href="https://www.facebook.com/Jscsezlipetsk" target="_blank"><img src="<?=SITE_TEMPLATE_PATH?>/img/face.png"></a>
+		<a href="https://twitter.com/Jscsezlipetsk" target="_blank">
+			<img src="<?=SITE_TEMPLATE_PATH?>/img/twitter.png">
+		</a>
+		<a href="https://www.youtube.com/channel/UCa73ezWaDhz5vS7k6d0dC3Q" target="_blank">
+			<img src="<?=SITE_TEMPLATE_PATH?>/img/youtube.png">
+		</a>
 	</div>
 </div>
