d34818dc5a5e34c9b8783b58717c85fd3f2629aa
kudaeva <angelforevor@mail.ru>
2015-09-30 13:35:39 +0300

    "Елецкая площадка ОЭЗ «Липецк»"

--- virtualnyy-tur/index.php
+++ virtualnyy-tur/index.php
@@ -3,7 +3,7 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Виртуальный тур");
 ?>
 
-<iframe src="http://ros.t198.topaz.fastwebserver.de/test5/output/1_3_1.html" width="100%" height="500px" allowfullscreen>
+<iframe src="http://ros.t198.topaz.fastwebserver.de/test7/output/1_3_1.html" width="100%" height="500px" allowfullscreen>
 	</iframe>
 
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
