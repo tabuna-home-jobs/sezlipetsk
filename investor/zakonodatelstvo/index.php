98fb3c180e5671c2170a22ebee19717abafa684a
Arpoksaj <putnik-prizrak@mail.ru>
2015-10-05 09:55:34 +0300

    изменения
    изменения

--- investor/zakonodatelstvo/index.php
+++ investor/zakonodatelstvo/index.php
@@ -22,7 +22,10 @@ $APPLICATION->SetTitle("Законодательство");
  <span style="font-size: 8pt;">24.09.2015</span><br>
 		</p>
 		<p>
-			 ЗАЯВКА на СОГЛАШЕНИЕ<br>
+			 ЗАЯВКА на СОГЛАШЕНИЕ
+		</p>
+		<p>
+			<br>
  <br>
  <a href="http://sezlipetsk/upload/zakonodatelstvo/%D0%9C%D0%98%D0%9D%D0%AD%D0%9A%20%E2%84%96%2075%20%D1%84%D0%BE%D1%80%D0%BC%D0%B0%20%D0%91%D0%9F.docx"><img width="24" alt="doc.png" src="/upload/medialibrary/466/4661a79ee6b7c0a9e87589a3252bec52.png" height="30" title="doc.png"></a>&nbsp;26кб<br>
 		</p>
@@ -33,13 +36,19 @@ $APPLICATION->SetTitle("Законодательство");
 			 МИНЭК № 75 форма БП<br>
 		</p>
 		<p>
+			<br>
+		</p>
+		<p>
  <a href="/upload/zakonodatelstvo/Рекомендации к оформлению бизнес-плана.docx"><img width="24" alt="doc.png" src="/upload/medialibrary/466/4661a79ee6b7c0a9e87589a3252bec52.png" height="30" title="doc.png"></a>&nbsp;12кб<br>
 		</p>
 		<p>
  <span style="font-size: 8pt;">24.09.2015</span>
 		</p>
 		<p>
-			 Рекомендации к оформлению бизнес-плана<br>
+			 Рекомендации к оформлению бизнес-плана
+		</p>
+		<p>
+			<br>
  <br>
  <a href="/upload/zakonodatelstvo/ФЗ от 08_08_2001 N 129-ФЗ.docx"><img width="24" alt="doc.png" src="/upload/medialibrary/466/4661a79ee6b7c0a9e87589a3252bec52.png" height="30" title="doc.png"></a>&nbsp;474кб
 		</p>
@@ -47,7 +56,10 @@ $APPLICATION->SetTitle("Законодательство");
  <span style="font-size: 8pt;">24.09.2015</span>
 		</p>
 		<p>
-			 ФЗ от 08_08_2001 N 129-ФЗ<br>
+			 ФЗ от 08_08_2001 N 129-ФЗ
+		</p>
+		<p>
+			<br>
  <br>
  <a href="/upload/zakonodatelstvo/ФЗ от 22_07_2005 N 116-ФЗ.docx"><img width="24" alt="doc.png" src="/upload/medialibrary/466/4661a79ee6b7c0a9e87589a3252bec52.png" height="30" title="doc.png"></a>&nbsp;66кб
 		</p>
@@ -57,7 +69,14 @@ $APPLICATION->SetTitle("Законодательство");
 		<p>
 			 ФЗ от 22_07_2005 N 116-ФЗ<br>
 		</p>
+		<p>
+			<br>
+		</p>
+ <img width="24" alt="doc.png" src="/upload/medialibrary/466/4661a79ee6b7c0a9e87589a3252bec52.png" height="30" title="doc.png">&nbsp;473кб<br>
+ <br>
+ <span style="font-size: 8pt;">05.10.2015</span>&nbsp;<br>
+ <br>
+ Федеральный закон от 08_08_2001 N 129-ФЗ (ред_ от 13_07_2015)<br>
 	</div>
  </section>
-</div>
- <br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+</div><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
