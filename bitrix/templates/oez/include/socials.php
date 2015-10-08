9666b2fd94ce41684d716cd62076f704cf898c82
kudaeva <angelforevor@mail.ru>
2015-10-08 11:42:16 +0300

    ссылка на фейсбук

--- bitrix/templates/oez/include/socials.php
+++ bitrix/templates/oez/include/socials.php
@@ -1,7 +1,7 @@
 <div class="col-lg-5 col-md-12 col-sm-6 col-xs-12 foot_item">
 	<div class="foot_name">Соц сети</div>
 	<div class="social">
-		<a href="https://www.facebook.com/Jscsezlipetsk" target="_blank"><img src="<?=SITE_TEMPLATE_PATH?>/img/face.png"></a>
+		<a href="https://www.facebook.com/Jscsezlipetsk-779023208894021/timeline/" target="_blank"><img src="<?=SITE_TEMPLATE_PATH?>/img/face.png"></a>
 		<a href="https://twitter.com/Jscsezlipetsk" target="_blank">
 			<img src="<?=SITE_TEMPLATE_PATH?>/img/twitter.png">
 		</a>
