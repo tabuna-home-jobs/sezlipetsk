e0ae4fbc0c3c1186f3d09bc8f5db5abfa81aee8c
Arpoksaj <putnik-prizrak@mail.ru>
2015-10-20 16:05:45 +0300

    Правки, цитата, пресрелиз
    Правки, цитата, пресрелиз

--- about/tsitaty-rukovoditeley/index.php
+++ about/tsitaty-rukovoditeley/index.php
@@ -1,111 +1,145 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Цитаты руководителей");
-?>
-
-	<div class="main">
-		<section class="container">
-			<!--Хеебный крош-->
-			<?$APPLICATION->IncludeComponent(
-				"custom:breadcrumb",
-				"",
-				Array(
-					"COMPONENT_TEMPLATE" => "oez",
-					"START_FROM" => "0",
-					"PATH" => "",
-					"SITE_ID" => "s1"
-				)
-			);
-			?>
-			<!--Хлебные крошки-->
-			<h1><?=$APPLICATION->GetTitle();?></h1>
-		</section>
-		<div class="citat_main">
-			<div class="citat_item">
-				<div class="container">
-					<div class="col-xs-12 col-sm-3 citat_item_img"><img src="<?=SITE_TEMPLATE_PATH?>/img/citat1.png"></div>
-					<div class="col-xs-12 col-sm-9 citat_item_body">
-						<div class="citat_item_text">
-							…экономическая эффективность фукционирования ОЭЗ обеспечивается, в основном за счет инвестиций резидентов ОЭЗ в Республике Татарстан и Липецкой области
-						</div>
-						<div class="citat_item_description">
-							<img src="<?=SITE_TEMPLATE_PATH?>/img/twoarrows.png">
-							<div class="citater"></div>
-							<div class="citater_2">
-								Из материалов Коллегии Счетной палаты РФ по результатам аудита Эффективности использования государственных средств, направленных на создание и развитие ОЭЗ
-							</div>
-
-						</div>
-					</div>
+?><div class="main">
+ <section class="container">
+	<!--Хеебный крош--> <?$APPLICATION->IncludeComponent(
+	"custom:breadcrumb",
+	"",
+	Array(
+		"COMPONENT_TEMPLATE" => "oez",
+		"START_FROM" => "0",
+		"PATH" => "",
+		"SITE_ID" => "s1"
+	)
+);?> <!--Хлебные крошки-->
+	<h1><?=$APPLICATION->GetTitle();?></h1>
+ </section>
+	<div class="citat_main">
+		<div class="citat_item">
+			<div class="container">
+				<div class="col-xs-12 col-sm-3 citat_item_img">
+					<img src="/bitrix/templates/oez/img/citat1.png">
 				</div>
-			</div>
-			<div class="citat_item">
-				<div class="container">
-					<div class="col-xs-12 col-sm-3 citat_item_img"><img src="<?=SITE_TEMPLATE_PATH?>/img/citat2.png"></div>
-					<div class="col-xs-12 col-sm-9 citat_item_body">
-						<div class="citat_item_text">
-							Компания ЙОКОХАМА Р.П.З. при выборе площадки для строительства шинного завода обследовала 21 регион на территории России и в конечном итоге выбор был сделан в пользу Липецкой особой экономической зоны. Причинами тому послужили: развитая инфраструктура, большой земельный участок, налоговые льготы. По соседству с особой экономической зоной располагается город Липецк с населением около полумиллиона человек. Это благоприятствует набору сотрудников на завод. Также особая экономическая зона – это государственный проект, поэтому здесь ощущается поддержка руководства Липецкой области».
-						</div>
-						<div class="citat_item_description">
-							<img src="<?=SITE_TEMPLATE_PATH?>/img/twoarrows.png">
-							<div class="citater"></div>
-							<div>Генеральный директор Yokohama</div>
-						</div>
+				<div class="col-xs-12 col-sm-9 citat_item_body">
+					<div class="citat_item_text">
+						 …экономическая эффективность фукционирования ОЭЗ обеспечивается, в основном за счет инвестиций резидентов ОЭЗ в Республике Татарстан и Липецкой области
 					</div>
-				</div>
-			</div>
-			<div class="ministr">
-				<div class="container">
-					<div class="col-xs-12 col-sm-9 citat_item_body ministr_body">
-						<div class="citat_item_text ministr_text">
-							Некоторые из первых ОЭЗ, созданных 7-8 лет назад, уже конкурентноспособны на мировом уровне: «Алабуга» в Татарстане и ОЭЗ в Липецке
+					<div class="citat_item_description">
+ <img src="/bitrix/templates/oez/img/twoarrows.png">
+						<div class="citater">
 						</div>
-						<div class="citat_item_description ministr_description">
-							<img src="<?=SITE_TEMPLATE_PATH?>/img/twoarrows_white.png">
-							<div class="citater">Олег Савельев</div>
-							<div class="citater_2">Заместитель Министра экономического развития РФ</div>
-						</div>
-					</div>
-					<div class="col-xs-12 col-sm-3 citat_item_img"><img src="<?=SITE_TEMPLATE_PATH?>/img/ministr.png"></div>
-				</div>
-			</div>
-			<div class="citat_item">
-				<div class="container">
-					<div class="col-xs-12 col-sm-3 citat_item_img"><img src="<?=SITE_TEMPLATE_PATH?>/img/citat4.png"></div>
-					<div class="col-xs-12 col-sm-9 citat_item_body">
-						<div class="citat_item_text">
-							В самой особой экономической зоне предоставляется готовая инфраструктура, действует профессиональная команда, которая оказывает содействие инвесторам в создании производств, поиске квалифицированных сотрудников, формировании  бизнес-кооперации с местным предпринимательским сообществом.
-
-							Оглядываясь назад сегодня и задавая себе вопрос, насколько правильным оказался наш выбор, мы можем с уверенностью утверждать, что приняли правильное решение. Наш проект был реализован в соответствии с планом….»
-
-						</div>
-						<div class="citat_item_description">
-							<img src="<?=SITE_TEMPLATE_PATH?>/img/twoarrows.png">
-							<div class="citater">Метин Калискан</div>
-							<div class="citater_2">Генеральный директор «Бекарт-Липецк»</div>
-						</div>
-					</div>
-				</div>
-			</div>
-
-			<div class="citat_item">
-				<div class="container">
-					<div class="col-xs-12 col-sm-3 citat_item_img"><img src="<?=SITE_TEMPLATE_PATH?>/img/citat5.png"></div>
-					<div class="col-xs-12 col-sm-9 citat_item_body">
-						<div class="citat_item_text">
-							…за семь лет работы проекты по созданию ОЭЗ в России среди них определились крепко ставшие на ноги. «Прежде всего, это промышленно-производственные, технико-внедренческие зоны — например, в Липецкой, Ульяновской, Псковской областях».
-						</div>
-						<div class="citat_item_description">
-							<img src="<?=SITE_TEMPLATE_PATH?>/img/twoarrows.png">
-							<div class="citater">Алексей Улюкаев</div>
-							<div class="citater_2">Министр экономического развития РФ</div>
+						<div class="citater_2">
+							 Из материалов Коллегии Счетной палаты РФ по результатам аудита Эффективности использования государственных средств, направленных на создание и развитие ОЭЗ
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-		</section>
-
+		<div class="citat_item">
+			<div class="container">
+				<div class="col-xs-12 col-sm-3 citat_item_img">
+					<img src="/bitrix/templates/oez/img/citat2.png">
+				</div>
+				<div class="col-xs-12 col-sm-9 citat_item_body">
+					<div class="citat_item_text">
+						 Компания ЙОКОХАМА Р.П.З. при выборе площадки для строительства шинного завода обследовала 21 регион на территории России и в конечном итоге выбор был сделан в пользу Липецкой особой экономической зоны. Причинами тому послужили: развитая инфраструктура, большой земельный участок, налоговые льготы. По соседству с особой экономической зоной располагается город Липецк с населением около полумиллиона человек. Это благоприятствует набору сотрудников на завод. Также особая экономическая зона – это государственный проект, поэтому здесь ощущается поддержка руководства Липецкой области».
+					</div>
+					<div class="citat_item_description">
+ <img src="/bitrix/templates/oez/img/twoarrows.png">
+						<div class="citater">
+						</div>
+						<div>
+							Генеральный директор Yokohama
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="ministr">
+			<div class="container">
+				<div class="col-xs-12 col-sm-9 citat_item_body ministr_body">
+					<div class="citat_item_text ministr_text">
+						 Некоторые из первых ОЭЗ, созданных 7-8 лет назад, уже конкурентноспособны на мировом уровне: «Алабуга» в Татарстане и ОЭЗ в Липецке
+					</div>
+					<div class="citat_item_description ministr_description">
+ <img src="/bitrix/templates/oez/img/twoarrows_white.png">
+						<div class="citater">
+							Олег Савельев
+						</div>
+						<div class="citater_2">
+							Заместитель Министра экономического развития РФ
+						</div>
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-3 citat_item_img">
+					<img src="/bitrix/templates/oez/img/ministr.png">
+				</div>
+			</div>
+		</div>
+		<div class="citat_item">
+			<div class="container">
+				<div class="col-xs-12 col-sm-3 citat_item_img">
+					<img src="/bitrix/templates/oez/img/citat4.png">
+				</div>
+				<div class="col-xs-12 col-sm-9 citat_item_body">
+					<div class="citat_item_text">
+						 В самой особой экономической зоне предоставляется готовая инфраструктура, действует профессиональная команда, которая оказывает содействие инвесторам в создании производств, поиске квалифицированных сотрудников, формировании бизнес-кооперации с местным предпринимательским сообществом. Оглядываясь назад сегодня и задавая себе вопрос, насколько правильным оказался наш выбор, мы можем с уверенностью утверждать, что приняли правильное решение. Наш проект был реализован в соответствии с планом….»
+					</div>
+					<div class="citat_item_description">
+ <img src="/bitrix/templates/oez/img/twoarrows.png">
+						<div class="citater">
+							Метин Калискан
+						</div>
+						<div class="citater_2">
+							Генеральный директор «Бекарт-Липецк»
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="citat_item">
+			<div class="container">
+				<div class="col-xs-12 col-sm-3 citat_item_img">
+					<img src="/bitrix/templates/oez/img/citat5.png">
+				</div>
+				<div class="col-xs-12 col-sm-9 citat_item_body">
+					<div class="citat_item_text">
+						 …за семь лет работы проекты по созданию ОЭЗ в России среди них определились крепко ставшие на ноги. «Прежде всего, это промышленно-производственные, технико-внедренческие зоны — например, в Липецкой, Ульяновской, Псковской областях».
+					</div>
+					<div class="citat_item_description">
+ <img src="/bitrix/templates/oez/img/twoarrows.png">
+						<div class="citater">
+							Алексей Улюкаев
+						</div>
+						<div class="citater_2">
+							Министр экономического развития РФ
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="citat_item">
+			<div class="container">
+				<div class="col-xs-12 col-sm-3 citat_item_img">
+					<img alt="Без имени.png" src="/upload/medialibrary/22b/22b2e40d7e5cc6e77941f1541ef18eee.png" title="Без имени.png">
+				</div>
+				<div class="col-xs-12 col-sm-9 citat_item_body">
+					<div class="citat_item_text">
+						 «Нам, как инвесторам нужен необычный подход и в особой экономической зоне «Липецк» мы его нашли!» <br>
+						Считаю, что в России существует две значимых особых экономических зоны. Это «Алабуга» в Татарстане и «Липецк». Хотя зон десятки, но эти две – безусловно лучшие. Здесь очень хорошая инфраструктура, блестящее территориальное расположение, поддержка со стороны губернатора Липецкой области, мощная команда управляющей компании. Мне это очень понравилось. Мы посетили заводы предприятий резидентов ОЭЗ «Липецк», побеседовали с их руководителями. Все они говорят о той помощи, что получили от руководства региона и особой экономической зоны в решении своих вопросов. Мы были в «Алабуге» и в ряде других особых экономических зон. На наш взгляд эта – лучшая.
+					</div>
+					<div class="citat_item_description">
+ <img src="/bitrix/templates/oez/img/twoarrows.png">
+						<div class="citater">
+							А.Г.Аганбегян
+						</div>
+						<div class="citater_2">
+							Академик РАН РФ
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
-
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+</div><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
