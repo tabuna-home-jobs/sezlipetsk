7f69b61eae738b4aa7f58de4d224863fca620159
kudaeva <angelforevor@mail.ru>
2015-11-25 12:36:33 +0300

    Целевые показатели рста EN

--- en/index.php
+++ en/index.php
@@ -162,13 +162,13 @@ $GLOBALS["arrFilterMain"] = array("PROPERTY_MAIN_VALUE" => 1);
   <!--Последняя новость на главной-->
   <?$APPLICATION->IncludeComponent(
 	"custom:news.list", 
-	"main_news",
+	"main_news", 
 	array(
 		"DISPLAY_DATE" => "Y",
 		"DISPLAY_PICTURE" => "Y",
 		"DISPLAY_PREVIEW_TEXT" => "Y",
-		"IBLOCK_TYPE" => "news",
-		"IBLOCK_ID" => "infoportal_news_s2",
+		"IBLOCK_TYPE" => "information",
+		"IBLOCK_ID" => "35",
 		"NEWS_COUNT" => "1",
 		"COMPONENT_TEMPLATE" => "main_news",
 		"SORT_BY1" => "ACTIVE_FROM",
@@ -190,7 +190,7 @@ $GLOBALS["arrFilterMain"] = array("PROPERTY_MAIN_VALUE" => 1);
 		"AJAX_OPTION_JUMP" => "N",
 		"AJAX_OPTION_STYLE" => "Y",
 		"AJAX_OPTION_HISTORY" => "N",
-		"AJAX_OPTION_ADDITIONAL" => "undefined",
+		"AJAX_OPTION_ADDITIONAL" => "",
 		"CACHE_TYPE" => "A",
 		"CACHE_TIME" => "36000000",
 		"CACHE_FILTER" => "N",
@@ -227,13 +227,84 @@ $GLOBALS["arrFilterMain"] = array("PROPERTY_MAIN_VALUE" => 1);
   <!--Последняя новость на главной-->
   <div class="col-lg-3 col-md-12 col-sm-12 col-xs-12 row block_stats">
    <div class="col-xs-12 news_name">Growth target</div>
-   <div class="col-xs-12 pokazateli"><div class="col-sm-6 col-xs-12 col-md-12 stat_img"><img src="<?=SITE_TEMPLATE_PATH?>/img/stat.jpg"></div>
+   <div class="col-xs-12 pokazateli"><div class="col-sm-6 col-xs-12 col-md-12 stat_img">
+
+		   <!--Слайдер Целевые показатели роста-->
+		   <? $APPLICATION->IncludeComponent(
+	"custom:news.list", 
+	"pokazat_rosta", 
+	array(
+		"IBLOCK_ID" => "37",
+		"COMPONENT_TEMPLATE" => "pokazat_rosta",
+		"IBLOCK_TYPE" => "Template",
+		"NEWS_COUNT" => "20",
+		"SORT_BY1" => "ACTIVE_FROM",
+		"SORT_ORDER1" => "DESC",
+		"SORT_BY2" => "SORT",
+		"SORT_ORDER2" => "ASC",
+		"FILTER_NAME" => "",
+		"FIELD_CODE" => array(
+			0 => "",
+			1 => "",
+		),
+		"PROPERTY_CODE" => array(
+			0 => "",
+			1 => "",
+		),
+		"CHECK_DATES" => "Y",
+		"DETAIL_URL" => "",
+		"AJAX_MODE" => "N",
+		"AJAX_OPTION_JUMP" => "N",
+		"AJAX_OPTION_STYLE" => "Y",
+		"AJAX_OPTION_HISTORY" => "N",
+		"AJAX_OPTION_ADDITIONAL" => "",
+		"CACHE_TYPE" => "A",
+		"CACHE_TIME" => "36000000",
+		"CACHE_FILTER" => "N",
+		"CACHE_GROUPS" => "N",
+		"PREVIEW_TRUNCATE_LEN" => "",
+		"ACTIVE_DATE_FORMAT" => "d.m.Y",
+		"SET_TITLE" => "N",
+		"SET_BROWSER_TITLE" => "N",
+		"SET_META_KEYWORDS" => "N",
+		"SET_META_DESCRIPTION" => "N",
+		"SET_LAST_MODIFIED" => "N",
+		"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+		"ADD_SECTIONS_CHAIN" => "N",
+		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+		"PARENT_SECTION" => "",
+		"PARENT_SECTION_CODE" => "",
+		"INCLUDE_SUBSECTIONS" => "Y",
+		"DISPLAY_DATE" => "Y",
+		"DISPLAY_NAME" => "Y",
+		"DISPLAY_PICTURE" => "Y",
+		"DISPLAY_PREVIEW_TEXT" => "Y",
+		"PAGER_TEMPLATE" => ".default",
+		"DISPLAY_TOP_PAGER" => "N",
+		"DISPLAY_BOTTOM_PAGER" => "Y",
+		"PAGER_TITLE" => "Новости",
+		"PAGER_SHOW_ALWAYS" => "N",
+		"PAGER_DESC_NUMBERING" => "N",
+		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+		"PAGER_SHOW_ALL" => "N",
+		"PAGER_BASE_LINK_ENABLE" => "N",
+		"SET_STATUS_404" => "N",
+		"SHOW_404" => "N",
+		"MESSAGE_404" => ""
+	),
+	FALSE
+);?>
+		   <!--Слайдер Целевые показатели роста-->
+		   <!--<img src="<?=SITE_TEMPLATE_PATH?>/img/stat.jpg"></div>
     <div class="col-sm-6 col-xs-12 col-md-12 stat_descript">
      <ul>
       <li><img src="<?=SITE_TEMPLATE_PATH?>/img/black_dot.png">implementation of the 1st phase of the project in Gryazi area</li>
       <li><img src="<?=SITE_TEMPLATE_PATH?>/img/red_dot.png">after the implementation of the 2nd phase in Yelets area</li>
      </ul>
-    </div></div>
+    </div>-->
+
+
+   </div>
   </div>
  </section>
 
