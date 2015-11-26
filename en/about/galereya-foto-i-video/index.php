d48948857535fdf7e659095a764475dda8533f26
kudaeva <angelforevor@mail.ru>
2015-11-26 09:23:09 +0300

    Галерея фото и видео EN

--- en/about/galereya-foto-i-video/index.php
+++ en/about/galereya-foto-i-video/index.php
@@ -1,4 +1,263 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Gallery photo Visual");
-?>Text here....<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+?>
+<script type="text/javascript">
+	$(document).ready(function(){
+		$('.fancybox').fancybox();
+	});
+</script>
+<div class="main container">
+	<section class="section_header">
+		<!--Хеебный крош-->
+		<?$APPLICATION->IncludeComponent(
+			"custom:breadcrumb",
+			"",
+			Array(
+				"COMPONENT_TEMPLATE" => "oez",
+				"START_FROM" => "0",
+				"PATH" => "",
+				"SITE_ID" => "s1"
+			)
+		);
+		?>
+		<!--Хлебные крошки-->
+		<h1><?=$APPLICATION->GetTitle();?></h1>
+	</section>
+	<section>
+		<div class="row galery_container">
+			<div class="col-md-5 col-sm-3">
+				<ul id="myTabs" class="nav nav-tabs nav_in_galery" role="tablist">
+					<li role="presentation" class="active">
+						<a href="#home" id="home-tab" role="tab" data-toggle="tab"
+						   aria-controls="home" aria-expanded="true">
+              <span>
+                <img src="<?=SITE_TEMPLATE_PATH?>/img/fotocamera.png">
+                <img class="active_img" src="<?=SITE_TEMPLATE_PATH?>/img/fotocamera_1.png">
+              </span>
+							Фотогалерея
+						</a>
+					</li>
+					<li role="presentation" class="">
+						<a href="#profile" role="tab" id="profile-tab" data-toggle="tab" aria-controls="profile" aria-expanded="false">
+            <span>
+                <img src="<?=SITE_TEMPLATE_PATH?>/img/videocamera.png">
+                <img class="active_img" src="<?=SITE_TEMPLATE_PATH?>/img/videocamera_1.png">
+            </span>
+							Видеогалерея
+						</a>
+					</li>
+
+				</ul>
+				<div class="hidden-xs">
+					<!--Основная фотография-->
+					<?$APPLICATION->IncludeComponent(
+						"custom:photogall_epta",
+						"main_photo",
+						array(
+							"IBLOCK_TYPE" => "information",
+							"IBLOCK_ID" => "40",
+							"ALBUM4EG_ID" => "358",
+							"COMPONENT_TEMPLATE" => "main_photo",
+							"SORT_BY1" => "ACTIVE_FROM",
+							"SORT_ORDER1" => "DESC",
+							"SORT_BY2" => "SORT",
+							"SORT_ORDER2" => "ASC",
+							"FILTER_NAME" => "",
+							"FIELD_CODE" => array(
+								0 => "",
+								1 => "",
+							),
+							"PROPERTY_CODE" => array(
+								0 => "",
+								1 => "",
+							),
+							"CHECK_DATES" => "Y",
+							"DETAIL_URL" => "",
+							"AJAX_MODE" => "N",
+							"AJAX_OPTION_JUMP" => "N",
+							"AJAX_OPTION_STYLE" => "Y",
+							"AJAX_OPTION_HISTORY" => "N",
+							"AJAX_OPTION_ADDITIONAL" => "undefined",
+							"CACHE_TYPE" => "A",
+							"CACHE_TIME" => "36000000",
+							"CACHE_FILTER" => "N",
+							"CACHE_GROUPS" => "Y",
+							"PREVIEW_TRUNCATE_LEN" => "",
+							"ACTIVE_DATE_FORMAT" => "d.m.Y",
+							"SET_TITLE" => "N",
+							"SET_BROWSER_TITLE" => "Y",
+							"SET_META_KEYWORDS" => "Y",
+							"SET_META_DESCRIPTION" => "Y",
+							"SET_LAST_MODIFIED" => "N",
+							"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+							"ADD_SECTIONS_CHAIN" => "N",
+							"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+							"PARENT_SECTION" => "",
+							"PARENT_SECTION_CODE" => "",
+							"INCLUDE_SUBSECTIONS" => "Y",
+							"DISPLAY_DATE" => "Y",
+							"DISPLAY_NAME" => "Y",
+							"DISPLAY_PICTURE" => "Y",
+							"DISPLAY_PREVIEW_TEXT" => "Y",
+							"PAGER_TEMPLATE" => ".default",
+							"DISPLAY_TOP_PAGER" => "N",
+							"DISPLAY_BOTTOM_PAGER" => "Y",
+							"PAGER_TITLE" => "Новости",
+							"PAGER_SHOW_ALWAYS" => "N",
+							"PAGER_DESC_NUMBERING" => "N",
+							"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+							"PAGER_SHOW_ALL" => "N",
+							"PAGER_BASE_LINK_ENABLE" => "N",
+							"SET_STATUS_404" => "N",
+							"SHOW_404" => "N",
+							"MESSAGE_404" => ""
+						),
+						false
+					);?>
+					<!--Основная фотография-->
+				</div>
+			</div>
+			<div class="col-md-7 col-sm-9 content_tabs_galery">
+
+
+				<div id="myTabContent" class="tab-content">
+					<!--Фотогалерея-->
+					<?$APPLICATION->IncludeComponent(
+						"custom:photogall_epta",
+						".default",
+						array(
+							"IBLOCK_TYPE" => "information",
+							"IBLOCK_ID" => "40",
+							"SORT_ORDER1" => "DESC",
+							"SORT_BY2" => "SORT",
+							"SORT_ORDER2" => "ASC",
+							"COMPONENT_TEMPLATE" => ".default",
+							"NEWS_COUNT" => "20",
+							"SORT_BY1" => "ACTIVE_FROM",
+							"FILTER_NAME" => "",
+							"FIELD_CODE" => array(
+								0 => "",
+								1 => "",
+							),
+							"PROPERTY_CODE" => array(
+								0 => "",
+								1 => "",
+							),
+							"CHECK_DATES" => "Y",
+							"DETAIL_URL" => "",
+							"AJAX_MODE" => "N",
+							"AJAX_OPTION_JUMP" => "N",
+							"AJAX_OPTION_STYLE" => "Y",
+							"AJAX_OPTION_HISTORY" => "N",
+							"AJAX_OPTION_ADDITIONAL" => "undefined",
+							"CACHE_TYPE" => "A",
+							"CACHE_TIME" => "36000000",
+							"CACHE_FILTER" => "N",
+							"CACHE_GROUPS" => "Y",
+							"PREVIEW_TRUNCATE_LEN" => "",
+							"ACTIVE_DATE_FORMAT" => "d.m.Y",
+							"SET_TITLE" => "N",
+							"SET_BROWSER_TITLE" => "N",
+							"SET_META_KEYWORDS" => "N",
+							"SET_META_DESCRIPTION" => "N",
+							"SET_LAST_MODIFIED" => "N",
+							"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+							"ADD_SECTIONS_CHAIN" => "N",
+							"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+							"PARENT_SECTION" => "",
+							"PARENT_SECTION_CODE" => "",
+							"INCLUDE_SUBSECTIONS" => "Y",
+							"DISPLAY_DATE" => "Y",
+							"DISPLAY_NAME" => "Y",
+							"DISPLAY_PICTURE" => "Y",
+							"DISPLAY_PREVIEW_TEXT" => "Y",
+							"PAGER_TEMPLATE" => ".default",
+							"DISPLAY_TOP_PAGER" => "N",
+							"DISPLAY_BOTTOM_PAGER" => "Y",
+							"PAGER_TITLE" => "Новости",
+							"PAGER_SHOW_ALWAYS" => "N",
+							"PAGER_DESC_NUMBERING" => "N",
+							"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+							"PAGER_SHOW_ALL" => "N",
+							"PAGER_BASE_LINK_ENABLE" => "N",
+							"SET_STATUS_404" => "N",
+							"SHOW_404" => "N",
+							"MESSAGE_404" => ""
+						),
+						false
+					);?>
+					<!--Фотогалерея-->
+					<!--Видеогалерея-->
+					<?$APPLICATION->IncludeComponent(
+						"custom:video_epta",
+						".default",
+						array(
+							"IBLOCK_TYPE" => "information",
+							"IBLOCK_ID" => "41",
+							"COMPONENT_TEMPLATE" => ".default",
+							"SORT_BY1" => "ACTIVE_FROM",
+							"SORT_ORDER1" => "DESC",
+							"SORT_BY2" => "SORT",
+							"SORT_ORDER2" => "ASC",
+							"FILTER_NAME" => "",
+							"FIELD_CODE" => array(
+								0 => "",
+								1 => "",
+							),
+							"PROPERTY_CODE" => array(
+								0 => "",
+								1 => "",
+							),
+							"CHECK_DATES" => "Y",
+							"DETAIL_URL" => "",
+							"AJAX_MODE" => "N",
+							"AJAX_OPTION_JUMP" => "N",
+							"AJAX_OPTION_STYLE" => "Y",
+							"AJAX_OPTION_HISTORY" => "N",
+							"AJAX_OPTION_ADDITIONAL" => "undefined",
+							"CACHE_TYPE" => "A",
+							"CACHE_TIME" => "36000000",
+							"CACHE_FILTER" => "N",
+							"CACHE_GROUPS" => "Y",
+							"PREVIEW_TRUNCATE_LEN" => "",
+							"ACTIVE_DATE_FORMAT" => "d.m.Y",
+							"SET_TITLE" => "N",
+							"SET_BROWSER_TITLE" => "Y",
+							"SET_META_KEYWORDS" => "Y",
+							"SET_META_DESCRIPTION" => "Y",
+							"SET_LAST_MODIFIED" => "N",
+							"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+							"ADD_SECTIONS_CHAIN" => "N",
+							"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+							"PARENT_SECTION" => "",
+							"PARENT_SECTION_CODE" => "",
+							"INCLUDE_SUBSECTIONS" => "Y",
+							"DISPLAY_DATE" => "Y",
+							"DISPLAY_NAME" => "Y",
+							"DISPLAY_PICTURE" => "Y",
+							"DISPLAY_PREVIEW_TEXT" => "Y",
+							"PAGER_TEMPLATE" => ".default",
+							"DISPLAY_TOP_PAGER" => "N",
+							"DISPLAY_BOTTOM_PAGER" => "Y",
+							"PAGER_TITLE" => "Новости",
+							"PAGER_SHOW_ALWAYS" => "N",
+							"PAGER_DESC_NUMBERING" => "N",
+							"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+							"PAGER_SHOW_ALL" => "N",
+							"PAGER_BASE_LINK_ENABLE" => "N",
+							"SET_STATUS_404" => "N",
+							"SHOW_404" => "N",
+							"MESSAGE_404" => ""
+						),
+						false
+					);?>
+					<!--Видеогалерея-->
+				</div>
+			</div>
+		</div>
+	</section>
+</div>
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+<!-- параметр "ALBUM4EG_ID" => "30",   - id раздела в альбоме-->
